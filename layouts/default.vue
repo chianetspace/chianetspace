6bf1284e67b71709fdd5ba2d703b60b5e6829a60
Clint.Network <me@clint.network>
2021-05-09 19:17:02 +0300

    Change hotjar id

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,7 +29,7 @@ export default {
 			h.hj = h.hj || function () {
 				(h.hj.q = h.hj.q || []).push(arguments)
 			};
-			h._hjSettings = {hjid: 1459403, hjsv: 6};
+			h._hjSettings = {hjid: 2391560, hjsv: 6};
 			a = o.getElementsByTagName('head')[0];
 			r = o.createElement('script');
 			r.async = 1;
