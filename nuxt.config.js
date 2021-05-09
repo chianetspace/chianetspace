d8d92f725c40bea1ffdc996211bea48b3d7a4b55
Clint.Network <me@clint.network>
2021-05-09 23:40:52 +0300

    Import bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
-		// '~/plugins/count-to.js'
+		'~/plugins/bootstrap.js'
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
