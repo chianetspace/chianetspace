a1da55bd10acb916f8bc77dd1755eb55deb7dc6f
litasio <33532075+litasio@users.noreply.github.com>
2021-05-10 15:31:25 +0300

    Update frequently-asked-questions.vue

--- pages/frequently-asked-questions.vue
+++ pages/frequently-asked-questions.vue
@@ -5,7 +5,7 @@
 				<h1 class="display-5">
 					Frequently Asked <strong class="fw-bold">Questions</strong>
 				</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<p>test</p>
 			</div>
 		</div>
 		<div class="row">
