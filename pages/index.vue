1cc111f3e2466b7afecc845b409a9d7e3cb074da
Clint.Network <me@clint.network>
2021-05-11 18:18:46 +0300

    Finishing home page

--- pages/index.vue
+++ pages/index.vue
@@ -3,61 +3,81 @@
 		<apexchart id="apexchart" :options="chartOptions" :series="series"
 				   class="position-fixed d-flex align-items-end bottom-0 start-0 end-0 w-100" height="421"
 				   style="z-index: 99" type="area"></apexchart>
-		<div class="container">
-			<div class="inner d-flex justify-content-center flex-column pb-5 align-items-center">
-				<h1 class="display-4">
-					Welcome to <strong class="fw-bold">Chia</strong>NetSpace<span>.com</span>
-				</h1>
-				<p class="lead text-center">
-					Dive into statistics and graphs of Chia (XCH) Proof of Space and<br/>Time historical netspace and
-					price data.
-				</p>
-				<h3 class="mt-4">Chia Netspace Now:</h3>
-				<h2 class="display-1 mb-0 text-success fw-bold">
-					<!--				v-anime="{textContent: [70000, 77000],-->
-					<!--				round: 1,-->
-					<!--				easing: 'linear',-->
-					<!--				duration: 2100,-->
-					<!--				round: 1,-->
-					<!--				}"-->
-					3.17<sub>000</sub>
-					<!--					<sub v-text="this.counter"></sub>-->
-					EiB
-				</h2>
-				<!--			<p class="lead text-uppercase">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>-->
-				<div class="row mt-5 w-75">
-					<div class="col-lg-4">
-						<div class="text-center">
+		<div class="container text-center pb-5">
+			<h1 class="display-4">
+				Welcome to <strong class="fw-bold">Chia</strong>NetSpace<span>.com</span>
+			</h1>
+			<p class="lead text-center">
+				Dive into statistics and graphs of Chia (XCH) Proof of Space and<br/>Time historical netspace and
+				price data.
+			</p>
+			<h3 class="mt-4">Chia Net Space Now:</h3>
+			<h2 class="display-1 mb-0 text-success fw-bold">
+				{{ summary.NetSpace.LargestWholeNumberDecimalValue.toFixed(0) }}<sub>.<span
+				v-anime="{textContent: [0, getDecimals(summary.NetSpace.LargestWholeNumberDecimalValue)],
+				round: 1,
+				easing: 'linear',
+				// duration: 2100,
+				round: 1,
+				}"></span></sub>
+				{{ summary.NetSpace.LargestWholeNumberBinarySymbol }}
+			</h2>
+			<!--			<p class="lead text-uppercase">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>-->
+			<div class="row mt-5 mb-4">
+				<div class="col-10 offset-1">
+					<div class="row">
+						<div class="col text-center">
 							<h6 class="fw-bold">Daily High / Daily Low</h6>
-							$1400.57 / $1049.04
+							{{
+								summary.MarketData.market_data.high_24h.usd.toLocaleString('en-US', {
+									style: 'currency',
+									currency: 'USD'
+								})
+							}}
+							/
+							{{
+								summary.MarketData.market_data.low_24h.usd.toLocaleString('en-US', {
+									style: 'currency',
+									currency: 'USD'
+								})
+							}}
 						</div>
-					</div>
-					<div class="col-lg-4 text-center">
-						<h6 class="fw-bold">Last Update</h6>
-						2021-05-09 16:00:03
-					</div>
-					<div class="col-lg-4 text-end">
-						<div class="text-center">
+						<div class="col text-center">
+							<h6 class="fw-bold">Last Update</h6>
+							{{ $moment(summary.LastUpdateDate).format('LLL') }}
+						</div>
+						<div class="col text-center">
 							<h6 class="fw-bold">Power Consumption</h6>
-							~ 4864 kW
+							≈ {{ (summary.NetSpace.LargestWholeNumberBinaryValue * 1.5).toFixed(0) }} kW
 						</div>
 					</div>
-				</div>
-				<div class="row mt-4 w-75">
-					<div class="col-lg-4">
-						<div class="text-center">
+					<div class="row mt-4">
+						<div class="col text-center">
 							<h6 class="fw-bold">All Time High</h6>
-							$1640.74
+							{{
+								summary.MarketData.market_data.ath.usd.toLocaleString('en-US', {
+									style: 'currency',
+									currency: 'USD'
+								})
+							}}
 						</div>
-					</div>
-					<div class="col-lg-4 text-center">
-						<h6 class="fw-bold">Chia (XCH) Price</h6>
-						$1185.44
-					</div>
-					<div class="col-lg-4 text-end">
-						<div class="text-center">
+						<div class="col text-center">
+							<h6 class="fw-bold">Chia (XCH) Price</h6>
+							{{
+								summary.MarketData.market_data.current_price.usd.toLocaleString('en-US', {
+									style: 'currency',
+									currency: 'USD'
+								})
+							}}
+						</div>
+						<div class="col text-center">
 							<h6 class="fw-bold">Daily Volume</h6>
-							Booming!
+							{{
+								summary.MarketData.market_data.total_volume.usd.toLocaleString('en-US', {
+									style: 'currency',
+									currency: 'USD'
+								})
+							}} XCH
 						</div>
 					</div>
 				</div>
@@ -72,8 +92,8 @@ export default {
 		return {
 			counter: 77238,
 			series: [{
-				name: "STOCK ABC",
-				data: ["274.99", "280.95", "295.59", "298.09", "319.18", "329.26", "348.34", "363.02", "391.34", "417.02", "469.85", "505.94", "574.24", "639.13", "722.27", "831.75", "881.49", "1006.14", "1166.58", "1232.21", "1375.04", "1561.34", "1743.03", "1881.04", "2186.16", "2480.45", "2704.22", "2916.48", "3145.26", "2905.08"]
+				name: "Net Space Growth",
+				data: []
 			}],
 			chartOptions: {
 				chart: {
@@ -135,22 +155,34 @@ export default {
 			},
 		}
 	},
+	async asyncData({$axios}) {
+		const summary = await $axios.$get('data/summary')
+		return {summary}
+	},
 	created() {
 		setInterval(() => this.counter++, 777);
+	},
+	mounted() {
+		this.$axios.$get('charts/net-space-growth').then((data) => {
+			this.series = [{
+				data: data
+			}];
+		});
+	},
+	methods: {
+		getDecimals: function (largestWholeNumberDecimalValue) {
+			let decimals = Math.abs(largestWholeNumberDecimalValue) - Math.floor(largestWholeNumberDecimalValue);
+			return parseInt(decimals.toString().substring(2, 5));
+		}
 	}
 }
 </script>
 
 <style scoped>
-.inner {
-	height: calc(100vh - 210px);
-}
-
 #apexchart {
 	z-index: 1;
 	user-select: none;
 	pointer-events: none;
-//opacity: 0.21;
 }
 
 .display-1 {
