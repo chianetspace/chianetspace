34d36b26de503c295baead848a9b7ec36d0e8923
Clint.Network <me@clint.network>
2021-05-11 18:18:31 +0300

    Add @nuxtjs/moment

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,8 @@ export default {
 
 	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 	buildModules: [
-		'nuxt-animejs'
+		'nuxt-animejs',
+		'@nuxtjs/moment',
 	],
 
 	// Modules: https://go.nuxtjs.dev/config-modules
