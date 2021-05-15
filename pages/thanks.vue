873bd6758a82c0edf69d35c7bb0265be3c77a556
Clint.Network <me@clint.network>
2021-05-16 01:22:39 +0300

    Create thanks page

---
+++ pages/thanks.vue
@@ -0,0 +1,16 @@
+<template>
+	<div
+		class="position-fixed top-0 bottom-0 start-0 end-0 bottom-0 pb-5 d-flex justify-content-center align-items-center flex-column">
+		<h1 class="display-5">Thank <strong class="fw-bold">You</strong></h1>
+		<p class="lead">You can follow the state of your order in the dashboard.</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'thanks'
+}
+</script>
+
+<style scoped>
+</style>
