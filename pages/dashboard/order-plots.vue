6bbdec88ff0bfde58dea815a0b092c612d9404e4
Clint.Network <me@clint.network>
2021-05-16 02:21:07 +0300

    Rename model to fit API and push to API

--- pages/dashboard/order-plots.vue
+++ pages/dashboard/order-plots.vue
@@ -21,23 +21,19 @@
 			<div class="mb-3">
 				<label class="form-label fw-bold" for="farmerprivatekey">Farmer Private Key <span
 					class="text-danger">*</span></label>
-				<input id="farmerprivatekey" v-model="model.farmerkey" class="form-control form-control-lg"
+				<input id="farmerprivatekey" v-model="model.farmerPrivateKey" class="form-control form-control-lg"
 					   placeholder="Enter your farmer private key here" required type="password"/>
 				<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 			</div>
 			<div class="mb-3">
 				<label class="form-label fw-bold" for="poolkey">Pool Key <span class="text-danger">*</span></label>
-				<input id="poolkey" v-model="model.poolkey" class="form-control form-control-lg"
+				<input id="poolkey" v-model="model.poolPrivateKey" class="form-control form-control-lg"
 					   placeholder="Enter the pool key here" required type="password"/>
 			</div>
 			<div class="mb-3">
-				<label class="form-label fw-bold" for="plotnumber">Plots Number: {{ model.count }}</label>
-				<input id="plotnumber" v-model="model.count" class="form-range" max="500" min="1" type="range">
+				<label class="form-label fw-bold" for="plotnumber">Plots Number: {{ model.plotsCount }}</label>
+				<input id="plotnumber" v-model="model.plotsCount" class="form-range" max="500" min="1" type="range">
 			</div>
-			<!--			<div class="mb-3 form-check">-->
-			<!--				<input type="checkbox" class="form-check-input" id="exampleCheck1">-->
-			<!--				<label class="form-check-label" for="exampleCheck1">Check me out</label>-->
-			<!--			</div>-->
 			<button class="btn btn-secondary" type="reset">Clear</button>
 			<button class="btn btn-primary ms-1" type="submit">
 				<i class="bx bxs-paper-plane me-1"></i>
@@ -52,8 +48,8 @@
 			<input name="currency" type="hidden" value="USD">
 			<input :value="totalAmount()" name="amountf" type="hidden">
 			<input name="item_name" type="hidden" value="Chia Plot">
-			<!--			<input type="hidden" name="success_url" value="@Url.Action("Proxies4GPayment", "Order", new { id = Model.OrderId }, "https")">-->
-			<input name="cancel_url" type="hidden" value="https://hypeproxy.io/dashboard/">
+			<input name="success_url" type="hidden" value="https://chianetspace.com/thanks">
+			<input name="cancel_url" type="hidden" value="https://chianetspace.com/dashboard/order-plots">
 		</form>
 	</div>
 </template>
@@ -65,15 +61,17 @@ export default {
 	data() {
 		return {
 			model: {
-				farmerkey: '',
-				poolkey: '',
-				count: 1
+				farmerPrivateKey: "string",
+				poolPrivateKey: "string",
+				plotsCount: 0
 			}
 		}
 	},
 	methods: {
 		createOrder: function () {
-			document.forms["coinpayments"].submit();
+			this.$axios.post('plots/order', this.model).then(response => {
+				document.forms["coinpayments"].submit();
+			})
 		},
 		totalAmount: function () {
 			return this.model.count * 12;
